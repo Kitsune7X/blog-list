ab0a79d2af8c48f81cdd3c945207ef80e1a96362
Kitsune7X <ryuizanagi@proton.me>
2025-11-23 02:44:04 +0900

    complete 4.3 and 4.4

---
+++ tests/list_helper.test.js
@@ -0,0 +1,84 @@
+import { test, describe } from 'node:test';
+import assert from 'node:assert';
+import listHelper from '../utils/list_helper.js';
+
+test('dummy returns one', () => {
+  const blogs = [];
+
+  const result = listHelper.dummy(blogs);
+  assert.strictEqual(result, 1);
+});
+
+// ---------- Total Likes test ----------
+describe('Total likes', () => {
+  const oneBlog = [
+    {
+      _id: '64b11aa51b54a676234d1801',
+      title: 'Hustle, Loyalty, Respect: The Champion’s Code',
+      author: 'John Cena',
+      url: 'https://wwe.com/articles/john-cena-code',
+      likes: 28,
+      __v: 0,
+    },
+  ];
+
+  const blogs = [
+    {
+      _id: '64b11aa51b54a676234d1801',
+      title: 'Hustle, Loyalty, Respect: The Champion’s Code',
+      author: 'John Cena',
+      url: 'https://wwe.com/articles/john-cena-code',
+      likes: 28,
+      __v: 0,
+    },
+    {
+      _id: '64b11ab71b54a676234d1802',
+      title: 'Art in Motion: The Charismatic Enigma',
+      author: 'Jeff Hardy',
+      url: 'https://wwe.com/articles/jeff-hardy-art-in-motion',
+      likes: 22,
+      __v: 0,
+    },
+    {
+      _id: '64b11ac81b54a676234d1803',
+      title: 'Voices: The Apex Predator’s Mind',
+      author: 'Randy Orton',
+      url: 'https://wwe.com/articles/randy-orton-apex-predator',
+      likes: 17,
+      __v: 0,
+    },
+    {
+      _id: '64b11ad91b54a676234d1804',
+      title: 'Flight of the Ultimate Underdog',
+      author: 'Rey Mysterio',
+      url: 'https://wwe.com/articles/rey-mysterio-underdog',
+      likes: 19,
+      __v: 0,
+    },
+    {
+      _id: '64b11aea1b54a676234d1805',
+      title: 'The Game: Mastering the Ring',
+      author: 'Triple H',
+      url: 'https://wwe.com/articles/triple-h-the-game',
+      likes: 23,
+      __v: 0,
+    },
+    {
+      _id: '64b11afb1b54a676234d1806',
+      title: 'Mind Games of The Deadman',
+      author: 'The Undertaker',
+      url: 'https://wwe.com/articles/undertaker-mind-games',
+      likes: 26,
+      __v: 0,
+    },
+  ];
+
+  test('of empty list is zero', () =>
+    assert.strictEqual(listHelper.totalLikes([]), 0));
+
+  test('when list has only one blog, equal the likes of that', () =>
+    assert.strictEqual(listHelper.totalLikes(oneBlog), 28));
+
+  test('of a bigger list is calculated right', () =>
+    assert.strictEqual(listHelper.totalLikes(blogs), 135));
+});
