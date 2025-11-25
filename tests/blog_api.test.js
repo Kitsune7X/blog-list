74306345b982ba176cc0baee140e83dfc1e7119b
Kitsune7X <ryuizanagi@proton.me>
2025-11-26 07:29:23 +0900

    refactor code

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -85,144 +85,144 @@ describe('When there are initially some blogs saved', () => {
     });
 
     // ---------- Test valid id but deleted ----------
-    test.only('fail with status 404 when blog does not exist', async () => {
+    test('fail with status 404 when blog does not exist', async () => {
       const nonExistingId = await blogHelper.nonExistingId();
 
       await api.get(`/api/blogs/${nonExistingId}`).expect(404);
     });
 
     // ---------- Test for invalid id ----------
-    test.only('fail with status 400 when id is invalid', async () => {
+    test('fail with status 400 when id is invalid', async () => {
       const invalidId = '23434311';
 
       await api.get(`/api/blogs/${invalidId}`).expect(400);
     });
   });
 
-  // describe('Post a new blog', () => )
-});
+  describe('Post a new blog', () => {
+    // ---------- Test Posting function ----------
+    test.only('Post a new blog works', async () => {
+      console.log('-- 5th test begin --');
+      console.log('----------------------------------');
 
-// ---------- Test Posting function ----------
-test('Post a new blog works', async () => {
-  console.log('-- 5th test begin --');
-  console.log('----------------------------------');
+      const newBlog = {
+        title: 'Rise of the Apex Predator',
+        author: 'Randy Orton',
+        url: 'https://wwe.com/articles/orton-apex',
+        likes: 9,
+      };
 
-  const newBlog = {
-    title: 'Rise of the Apex Predator',
-    author: 'Randy Orton',
-    url: 'https://wwe.com/articles/orton-apex',
-    likes: 9,
-  };
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+      const blogsAtEnd = await blogHelper.blogsInDb();
 
-  const blogsAtEnd = await blogHelper.blogsInDb();
+      // Check if the length of the new array === initialBlogs + 1
+      assert.strictEqual(blogsAtEnd.length, blogHelper.initialBlogs.length + 1);
 
-  // Check if the length of the new array === initialBlogs + 1
-  assert.strictEqual(blogsAtEnd.length, blogHelper.initialBlogs.length + 1);
+      // Check if the posted Blog is the same as `newBlog`
+      const titles = blogsAtEnd.map((blog) => blog.title);
 
-  // Check if the posted Blog is the same as `newBlog`
-  const titles = blogsAtEnd.map((blog) => blog.title);
+      assert.strictEqual(titles.includes(newBlog.title), true);
+    });
 
-  assert.strictEqual(titles.includes(newBlog.title), true);
-});
+    // ---------- Test for invalid blog post ----------
+    test.only('Blog with empty body will not be added', async () => {
+      console.log('-- 6th test begin --');
+      console.log('----------------------------------');
 
-// ---------- Test for invalid blog post ----------
-test('Blog with empty body will not be added', async () => {
-  console.log('-- 6th test begin --');
-  console.log('----------------------------------');
+      const blogWithoutBody = {};
 
-  const blogWithoutBody = {};
+      await api.post('/api/blogs').send(blogWithoutBody).expect(400);
 
-  await api.post('/api/blogs').send(blogWithoutBody).expect(400);
+      // Verify that the amount of blogs does not change
+      const blogsAtEnd = await Blog.find({});
 
-  // Verify that the amount of blogs does not change
-  const blogsAtEnd = await Blog.find({});
+      assert.strictEqual(blogsAtEnd.length, blogHelper.initialBlogs.length);
+    });
 
-  assert.strictEqual(blogsAtEnd.length, blogHelper.initialBlogs.length);
-});
+    // ---------- Test for missing `likes` property ----------
+    // If the `likes` property is missing from `req`, default to 0
+    test.only('When "Likes" is missing, default to 0', async () => {
+      console.log('-- 7th test begin --');
+      console.log('----------------------------------');
 
-// ---------- Test for missing `likes` property ----------
-// If the `likes` property is missing from `req`, default to 0
-test('When "Likes" is missing, default to 0', async () => {
-  console.log('-- 7th test begin --');
-  console.log('----------------------------------');
+      const blogWithoutLikes = {
+        title: "The Game's Evolution",
+        author: 'Triple H',
+        url: 'https://wwe.com/articles/triple-h-evolution',
+      };
 
-  const blogWithoutLikes = {
-    title: "The Game's Evolution",
-    author: 'Triple H',
-    url: 'https://wwe.com/articles/triple-h-evolution',
-  };
+      // Test if the blog is successfully posted first
+      await api
+        .post('/api/blogs')
+        .send(blogWithoutLikes)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-  // Test if the blog is successfully posted first
-  await api
-    .post('/api/blogs')
-    .send(blogWithoutLikes)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+      // Check if the likes is 0
+      const addedBlog = (await Blog.find({})).at(-1);
+      assert.strictEqual(addedBlog.likes, 0);
+    });
 
-  // Check if the likes is 0
-  const addedBlog = (await Blog.find({})).at(-1);
-  assert.strictEqual(addedBlog.likes, 0);
-});
+    // ---------- Test for missing `Title` ----------
+    test.only('Blog without "Title" will not be added', async () => {
+      console.log('-- 8th test begin --');
+      console.log('----------------------------------');
 
-// ---------- Test for missing `Title` ----------
-test('Blog without "Title" will not be added', async () => {
-  console.log('-- 8th test begin --');
-  console.log('----------------------------------');
+      const blogWithoutTitle = {
+        author: 'The Undertaker',
+        url: 'https://wwe.com/articles/undertaker-aura',
+        likes: 31,
+      };
 
-  const blogWithoutTitle = {
-    author: 'The Undertaker',
-    url: 'https://wwe.com/articles/undertaker-aura',
-    likes: 31,
-  };
+      await api.post('/api/blogs').send(blogWithoutTitle).expect(400);
 
-  await api.post('/api/blogs').send(blogWithoutTitle).expect(400);
+      // Verify that the amount of blogs does not change
+      const blogsAtEnd = await Blog.find({});
 
-  // Verify that the amount of blogs does not change
-  const blogsAtEnd = await Blog.find({});
+      assert.strictEqual(blogsAtEnd.length, blogHelper.initialBlogs.length);
+    });
 
-  assert.strictEqual(blogsAtEnd.length, blogHelper.initialBlogs.length);
-});
+    // ---------- Test for missing Author ----------
+    test.only('Blog without "Author" will not be added', async () => {
+      console.log('-- 9th test begin --');
+      console.log('----------------------------------');
 
-// ---------- Test for missing Author ----------
-test('Blog without "Author" will not be added', async () => {
-  console.log('-- 9th test begin --');
-  console.log('----------------------------------');
+      const blogWithoutAuthor = {
+        title: "The Deadman's Aura",
+        url: 'https://wwe.com/articles/undertaker-aura',
+        likes: 31,
+      };
 
-  const blogWithoutAuthor = {
-    title: "The Deadman's Aura",
-    url: 'https://wwe.com/articles/undertaker-aura',
-    likes: 31,
-  };
+      await api.post('/api/blogs').send(blogWithoutAuthor).expect(400);
 
-  await api.post('/api/blogs').send(blogWithoutAuthor).expect(400);
+      const blogsAtEnd = await Blog.find({});
 
-  const blogsAtEnd = await Blog.find({});
+      assert.strictEqual(blogsAtEnd.length, blogHelper.initialBlogs.length);
+    });
 
-  assert.strictEqual(blogsAtEnd.length, blogHelper.initialBlogs.length);
-});
+    // ---------- Test for missing Url ----------
+    test.only('Blog without "Url" will not be added', async () => {
+      console.log('-- 10th test begin --');
+      console.log('----------------------------------');
 
-// ---------- Test for missing Url ----------
-test('Blog without "Url" will not be added', async () => {
-  console.log('-- 10th test begin --');
-  console.log('----------------------------------');
+      const blogWithoutUrl = {
+        title: "The Deadman's Aura",
+        author: 'The Undertaker',
+        likes: 31,
+      };
 
-  const blogWithoutUrl = {
-    title: "The Deadman's Aura",
-    author: 'The Undertaker',
-    likes: 31,
-  };
+      await api.post('/api/blogs').send(blogWithoutUrl).expect(400);
 
-  await api.post('/api/blogs').send(blogWithoutUrl).expect(400);
+      const blogsAtEnd = await Blog.find({});
 
-  const blogsAtEnd = await Blog.find({});
-
-  assert.strictEqual(blogsAtEnd.length, blogHelper.initialBlogs.length);
+      assert.strictEqual(blogsAtEnd.length, blogHelper.initialBlogs.length);
+    });
+  });
 });
 
 // ---------- Close the connection ----------
